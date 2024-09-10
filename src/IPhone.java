8d0280f99e36a9ee834cd9f7fb86e039ee90b11a
hugo <hugo.a.da.silva@gmail.com>
2024-09-10 20:20:31 -0300

    ModelagemIPhone

--- src/IPhone.java
+++ src/IPhone.java
@@ -1,18 +1,38 @@
 public class IPhone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
 
+    static String ligar;
+    static String musica;
+    static String url;
+
+    public static String getMusica() {
+        return musica;
+    }
+
+    public static void setMusica(String musica) {
+        IPhone.musica = musica;
+    }
+
+    public static String getUrl() {
+        return url;
+    }
+
+    public static void setUrl(String url) {
+        IPhone.url = url;
+    }
+
     @Override
     public void tocar() {
-        System.out.println("Tocando musica");
+        System.out.println("Tocando música");
     }
 
     @Override
     public void pausar() {
-        System.out.println("Pausando musica");
+        System.out.println("Pausando música");
     }
 
     @Override
     public void selecionarMusica(String musica) {
-        System.out.println("Selecionando musica");
+        System.out.println("Selecionando música: " + IPhone.musica);
     }
     
     @Override
@@ -27,7 +47,7 @@ public class IPhone implements AparelhoTelefonico, NavegadorInternet, Reprodutor
 
     @Override
     public void ligar(String numero) {
-        System.out.println("Fazendo ligação");
+        System.out.println("Fazendo ligação: " + IPhone.ligar);
     }
 
     @Override
@@ -42,6 +62,6 @@ public class IPhone implements AparelhoTelefonico, NavegadorInternet, Reprodutor
 
     @Override
     public void exibirPagina(String url) {
-        System.out.println("Exibindo página internet");
+        System.out.println("Exibindo página internet: " + IPhone.url);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,28 @@
 public class Main {
+    @SuppressWarnings("static-access")
     public static void main(String[] args) {
 
         IPhone IPhone = new IPhone();
-       
+
+        IPhone.ligar = "(11)5555-1234";
         System.out.println("");
         System.out.println("TELEFONE");
         IPhone.atender();
         IPhone.iniciarCorreioVoz();
-        IPhone.ligar(null);
-
+        IPhone.ligar("");
+        
+        IPhone.musica = "Metallica - One";
         System.out.println("");
         System.out.println("IPOD");
         IPhone.tocar();
         IPhone.pausar();
-        IPhone.selecionarMusica(null);
+        IPhone.selecionarMusica("");
 
+        IPhone.url = "www.google.com.br";
         System.out.println("");
         System.out.println("SAFARI");
         IPhone.adicionarNovaAba();
         IPhone.atualizarPagina();
-        IPhone.exibirPagina(null);
+        IPhone.exibirPagina("");
     }
 }
